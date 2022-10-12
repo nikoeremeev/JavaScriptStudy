8989c5cba46cd909d0bf25fff2a26d25ec5c5ef1
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-10-12 20:49:46 +0300

    task

---
+++ lesson_8/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let fitlerPopup = document.querySelector('.filterPopup');
+let fitlerLabel = document.querySelector('.filterLabel');
+let filterIcon = document.querySelector('.filterIcon');
+
+fitlerLabel.addEventListener('click', function() {
+    fitlerPopup.classList.toggle('hidden');
+    fitlerLabel.classList.toggle('filterLabelPink');
+    filterIcon.classList.toggle('filterIconPink');
+
+    if (filterIcon.getAttribute('src') === 'images/filter.svg') {
+        filterIcon.setAttribute('src', 'images/filterHover.svg')
+    } else {
+        filterIcon.setAttribute('src', 'images/filter.svg')
+    }
+});
+
+let filterHeaders = document.querySelectorAll('.filterCategoryHeader');
+filterHeaders.forEach(function(header) {
+    header.addEventListener('click', function(event) {
+        event.target.nextElementSibling.classList.toggle('hidden');
+    })
+});
+
+let filterSizes = document.querySelector('.filterSizes');
+let filterSizeWrap = document.querySelector('.filterSizeWrap');
+filterSizeWrap.addEventListener('click', function() {
+    filterSizes.classList.toggle('hidden');
+});
